9701a191bccf5b49aec87ce616dc155adde9b5e6
Robert Grandin <grandin.robert@gmail.com>
2017-07-16 16:57:24 -0400

    Add creation script to build and launch the image.

---
+++ create.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# https://hub.docker.com/r/linuxserver/unifi/
+# https://github.com/linuxserver/docker-unifi
+
+
+# To pull latest: docker pull linuxserver/unifi
+# Better success: 
+#   1.  git clone https://github.com/linuxserver/docker-unifi.git
+#   2.  Place this file within the cloned directory
+#   3.  Run this file.
+
+
+
+
+
+# Make the docker image, directing the resulting hash to a file
+# for later reference.
+docker build -t unifi . 
+
+# > ImageHash.txt
+
+HOST_DIR=/var/ubnt/container
+
+# Create the container
+docker create \
+  --name=unifi \
+  -v $HOST_DIR:/config \
+  -e PGID=1001 -e PUID=1001  \
+  -p 8080:8080 \
+  -p 8081:8081 \
+  -p 8443:8443 \
+  -p 8843:8843 \
+  -p 8880:8880 \
+  unifi   #      > ContainerHash.txt
+
+
+
+# Start the container.
+docker start unifi
+#docker run \
+#  -p 8080:8080 \
+#  -p 8081:8081 \
+#  -p 8443:8443 \
+#  -p 8843:8843 \
+#  -p 8880:8880 \
+#  --rm \
+#  unifi
+
